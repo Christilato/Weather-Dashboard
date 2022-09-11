3f3dbc02cabc51b377997057de6fdac488b6da18
Christi Lato <christilato@Christis-MacBook-Air.local>
2022-09-11 16:36:29 -0500

    Jumbotron added and beginning to format html file. Added in the API in script for the Weather Data

---
+++ Assets/Java/script.js
@@ -0,0 +1,7 @@
+var APIKey = "16a6e0fc0c7e5f13cae374b5adfdbd66";
+var city;
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+fetch(queryURL)
+//You'll need to adjust your application to accept user input, to store in the city variable that you've created.
+
